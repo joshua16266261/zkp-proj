d45f46ffdb5478f7fcff8c6e2b898e81bd880916
Joshua Chen <joshua6261@att.net>
2023-05-12 16:03:35 -0700

    Finished protocol 0

--- circuits/protocol0/main.go
+++ circuits/protocol0/main.go
@@ -2,145 +2,97 @@ package protocol0
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
+	"reflect"
+	"time"
 
 	"github.com/consensys/gnark-crypto/accumulator/merkletree"
-	"github.com/consensys/gnark-crypto/ecc"
+	bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
 	"github.com/consensys/gnark-crypto/hash"
-	"github.com/consensys/gnark/backend/groth16"
-	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/frontend/cs/r1cs"
-	"github.com/consensys/gnark/std/accumulator/merkle"
-	"github.com/consensys/gnark/std/hash/mimc"
 )
 
-type Circuit struct {
-	MerkleProof merkle.MerkleProof `gnark:",public"`
-	ProofIndex  frontend.Variable  // TODO: Should this be public or private?
+func getFieldElements(pattern string) [][]byte {
+	// GetFieldElements converts each character of a string pattern into a field element
+	var fieldElems [][]byte
+
+	blockSize := bn254.BlockSize
+
+	for _, char := range pattern {
+		b := make([]byte, blockSize)
+		b[blockSize-1] = byte(char)
+		fieldElems = append(fieldElems, b)
+	}
+	return fieldElems
 }
 
-func (circuit *Circuit) Define(api frontend.API) error {
-	hFunc, _ := mimc.NewMiMC(api)
-	circuit.MerkleProof.VerifyProof(api, &hFunc, circuit.ProofIndex)
+func getHash(pattern string) ([]byte, error) {
+	// GetHash hashes a string pattern into a single field element
 
-	// TODO: Verify that the value opened from the merkle tree is correct or something
+	converted := getFieldElements(pattern)
+	var flattened []byte
+	for _, elem := range converted {
+		flattened = append(flattened, elem...)
+	}
 
-	return nil
+	return bn254.Sum(flattened)
 }
 
 func main() {
-	// Define pattern
-	field := ecc.BN254.ScalarField()
-	modNbBytes := len(field.Bytes())
-	var buf bytes.Buffer
-	numLeaves := uint16(4)
-	for i := uint16(0); i < numLeaves; i++ {
-		// FIXME: Each element in the committed vector is a field element of length modNbBytes bytes
-		// How do we commit to a list of patterns?
-		leaf := i
-		b := new(bytes.Buffer)
-		err := binary.Write(b, binary.LittleEndian, leaf)
+	// Define patterns
+	stringPatterns := []string{"abcdefghijkl", "de*"}
+	var hashedPatterns [][]byte
+	for _, pattern := range stringPatterns {
+		hashedPattern, err := getHash(pattern)
 		if err != nil {
-			fmt.Println("binary.Write failed:", err)
+			fmt.Println("Error hashing pattern:", err)
+			return
 		}
-		buf.Write(make([]byte, modNbBytes-b.Len()))
-		buf.Write(b.Bytes())
+		hashedPatterns = append(hashedPatterns, hashedPattern)
+	}
+
+	// Build merkle proof
+	start := time.Now()
+	var buf bytes.Buffer
+	for _, pattern := range hashedPatterns {
+		buf.Write(pattern)
 	}
 
 	hGo := hash.MIMC_BN254.New()
 	segmentSize := 32
-	proofIndex := uint64(1)
+	proofIndex := uint64(0)
 	merkleRoot, proofPath, _, err := merkletree.BuildReaderProof(bytes.NewReader(buf.Bytes()), hGo, segmentSize, proofIndex)
-	fmt.Println("merkleRoot 1", merkleRoot)
+	// The actual value of the leaf is proofPath[0]
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	duration := time.Since(start)
+	fmt.Println("Prover time:", duration)
 
-	depth := 2
+	start = time.Now()
+	isValidProof := merkletree.VerifyProof(hGo, merkleRoot, proofPath, proofIndex, uint64(len(stringPatterns)))
+	if !isValidProof {
+		fmt.Println("failed to verify proof")
+		return
+	}
 
-	var circuit Circuit
-	circuit.MerkleProof.Path = make([]frontend.Variable, depth+1)
-
-	r1cs, err := frontend.Compile(field, r1cs.NewBuilder, &circuit)
+	// Define client string
+	clientString := "abcdefghijkl"
+	hashedClientString, err := getHash(clientString)
 	if err != nil {
-		fmt.Printf("Compile failed : %v\n", err)
+		fmt.Println("Error hashing clientString:", err)
 		return
 	}
 
-	pk, vk, err := groth16.Setup(r1cs)
-	if err != nil {
-		fmt.Printf("Setup failed\n")
+	patternEqualsString := reflect.DeepEqual(hashedClientString, proofPath[0])
+	if !patternEqualsString {
+		fmt.Println("client string and pattern do not match")
 		return
 	}
+	duration = time.Since(start)
+	fmt.Println("Verifier time:", duration)
 
-	var merkleProof merkle.MerkleProof
-	merkleProof.RootHash = merkleRoot
-
-	merkleProof.Path = make([]frontend.Variable, depth+1)
-	for i := 0; i < depth+1; i++ {
-		merkleProof.Path[i] = proofPath[i]
-	}
-
-	assignment := &Circuit{
-		MerkleProof: merkleProof,
-		ProofIndex:  proofIndex,
-	}
-
-	witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
-	proof, err := groth16.Prove(r1cs, pk, witness)
-	if err != nil {
-		fmt.Printf("Prove failed: %v\n", err)
-		return
-	}
-
-	///////// Assignment for different index /////////
-	proofIndex = uint64(2)
-	merkleRoot, proofPath, _, err = merkletree.BuildReaderProof(bytes.NewReader(buf.Bytes()), hGo, segmentSize, proofIndex)
-	fmt.Println("merkleRoot 2", merkleRoot)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var falseMerkleProof merkle.MerkleProof
-	falseMerkleProof.RootHash = merkleRoot
-
-	falseMerkleProof.Path = make([]frontend.Variable, depth+1)
-	for i := 0; i < depth+1; i++ {
-		falseMerkleProof.Path[i] = proofPath[i]
-	}
-
-	falseAssignment := &Circuit{
-		MerkleProof: falseMerkleProof,
-		ProofIndex:  proofIndex,
-	}
-
-	falseWitness, err := frontend.NewWitness(falseAssignment, ecc.BN254.ScalarField())
-	falseProof, err := groth16.Prove(r1cs, pk, falseWitness)
-	if err != nil {
-		fmt.Printf("Prove failed: %v\n", err)
-		return
-	}
-
-	///////// Public witness and verification /////////
-
-	publicWitness, err := witness.Public()
-	err = groth16.Verify(proof, vk, publicWitness)
-	if err != nil {
-		fmt.Printf("verification failed: %v\n", err)
-		return
-	}
-	fmt.Printf("verification succeded\n")
-
-	err = groth16.Verify(falseProof, vk, publicWitness)
-	if err == nil {
-		fmt.Printf("False verification should have failed but did not")
-		return
-	}
-	fmt.Printf("verification failed successfully\n")
-
+	fmt.Println("Verification succeeded")
 }
 
 func Main() {
